7ca842accce01198786ce6b77ffc60e9ef8f09c3
Lev <levdevuk@gmail.com>
2024-04-30 19:24:16 +0100

    Exercise 2 | My initial solution

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,14 +2,32 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-
+import GuessInput from '../GuessInput';
+import GuessResults from '../GuessResults';
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSubmitGuess(tentativeGuess) {
+    const newGuess = {
+      tentativeGuess,
+      id: crypto.randomUUID(),
+    };
+
+    const nextGuesses = [...guesses, newGuess];
+    setGuesses(nextGuesses);
+  }
+
+  return (
+    <>
+      <GuessResults guesses={guesses} />
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
+    </>
+  );
 }
 
 export default Game;
---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function GuessInput({ handleSubmitGuess }) {
+  const [tentativeGuess, setTentativeGuess] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleSubmitGuess(tentativeGuess);
+    setTentativeGuess('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="guess-input-wrapper">
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        required
+        value={tentativeGuess}
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        onChange={(event) => {
+          const nextGuess = event.target.value.toUpperCase();
+          setTentativeGuess(nextGuess);
+        }}
+        id="guess-input"
+        type="text"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
